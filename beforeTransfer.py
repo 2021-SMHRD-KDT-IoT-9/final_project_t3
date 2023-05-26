06cc8e4155aedb6b274f6c42a9f418aa4b2029ec
이연규 <122160394+Yg-Rep@users.noreply.github.com>
2023-05-26 17:41:26 +0900

    Add files via upload
    before Transfer 파일입니다. 
    before 사진을 스프링부트로 옮기는 코드가 적혀있습니다.

---
+++ beforeTransfer.py
@@ -0,0 +1,34 @@
+
+##before
+
+# http 통신 관련
+import requests
+
+# picam 관련
+import os
+
+   
+# 이미지를 찍고 전송해는 함수
+def imageTransfer():
+    url = "http://59.0.234.211:8087/before"
+# 카메라를 동작을 위한 설정
+
+    # 파일 경로
+    route = "/home/yg/webapp/static/before.jpg" 
+   
+    f = open(route,"rb") # 이미지 파일 열기
+    img = f.read() # 이미지파일 변수에 저장
+
+    print(img)
+   
+    response = requests.post(url, files={ # 이미지를 전송하기 위한 설정
+        "img" : img,
+        "type" : "file",
+        "name" : "before.jpg"
+        })
+    f.close()
+    print(response.status_code)
+  
+
+imageTransfer()
+print("imageTransfer")
