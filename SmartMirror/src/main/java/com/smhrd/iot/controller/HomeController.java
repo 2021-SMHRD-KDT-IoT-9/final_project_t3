c7b5cc12516cc9d3c45aa3713bbda3e7e2f04f8a
Jaeuk <rainbowski@naver.com>
2023-05-30 14:30:49 +0900

    css 적용

--- SmartMirror/src/main/java/com/smhrd/iot/controller/HomeController.java
+++ SmartMirror/src/main/java/com/smhrd/iot/controller/HomeController.java
@@ -11,6 +11,28 @@ public class HomeController {
 		return "main";
 	}
 	
+	@GetMapping("/video")
+	public String video() {
+		return "video";
+	}
+	
+	@GetMapping("/image")
+	public String image() {
+		return "image";
+	}	
+	
+	@GetMapping("/hair")
+	public String hair() {
+		return "hair";
+	}	
+	
+	@GetMapping("/style")
+	public String style() {
+		return "style";
+	}
+	
+	
+	// ------------------------test-------------------------
 	@GetMapping("/uptest")
 	public String testmain() {
 		return "ztest-main";
